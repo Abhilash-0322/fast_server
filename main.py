f4b90ab5ab6afb60ba9aea89d512b77b5c71b4cc
Abhilash Maurya <136215541+Abhilash-0322@users.noreply.github.com>
2024-12-01 00:55:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     return ("""<h1>Hello, world!</h1>
-    <h2>Hello, Again</h2>""")
+    <h2>Hello, Again</h2>
+    <script>document.getElementsByTagName("h1").style.color="red";</script>""")
